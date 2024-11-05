5d1126e92f744af3fa0cb51540b83ac4138b1a14
caeblox <code@caeblox.org>
2024-11-05 21:28:47 +0000

    src/similarity

--- src/similarity_metrics.py
+++ src/similarity_metrics.py
@@ -1,25 +1,34 @@
-import numpy as np
 import torch
+import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
-def calculate_sdf_similarity(tensor1, tensor2, neighbors=10):
+def sign_distance_function(tensor1, tensor2, n_neighbors=10):
     """
-    Calculates similarity between two tensors representing point clouds 
-    using a nearest-neighbor SDF approach.
+    Calculate similarity based on a nearest neighbors approach in the SDF space.
+    """
+    points1 = tensor1.cpu().numpy()
+    points2 = tensor2.cpu().numpy()
 
-    Parameters:
-        tensor1, tensor2 (torch.Tensor): Point clouds.
-        neighbors (int): Number of neighbors for similarity calculation.
-    
-    Returns:
-        float: Similarity score.
-    """
-    points1, points2 = tensor1.cpu().numpy(), tensor2.cpu().numpy()
-    nn1 = NearestNeighbors(n_neighbors=neighbors).fit(points1)
-    nn2 = NearestNeighbors(n_neighbors=neighbors).fit(points2)
+    nn1 = NearestNeighbors(n_neighbors=n_neighbors).fit(points1)
+    nn2 = NearestNeighbors(n_neighbors=n_neighbors).fit(points2)
 
     distances_1to2, _ = nn1.kneighbors(points2, return_distance=True)
     distances_2to1, _ = nn2.kneighbors(points1, return_distance=True)
 
-    avg_distance = (distances_1to2.mean() + distances_2to1.mean()) / 2.0
-    return 1.0 / (1.0 + avg_distance + 1e-8)
+    sdf_distance = (distances_1to2.mean() + distances_2to1.mean()) / 2.0
+    epsilon = 1e-8
+    similarity_score = 1.0 / (1.0 + sdf_distance + epsilon)
+    
+    return similarity_score
+
+def calculate_similarity(tensor1, tensor2):
+    """
+    Calculate similarity using the sign distance function.
+    """
+    return sign_distance_function(tensor1, tensor2)
+
+def calculate_network_similarity_score(matrix_data):
+    """
+    Calculate the average similarity score of the network.
+    """
+    return matrix_data.stack().mean()
